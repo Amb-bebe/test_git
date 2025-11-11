479b438fb835b2cc87bd8ec2122ffd41f1c6ce81
Amb-bebe <ammb2311@gmail.com>
2025-11-11 12:44:30 +0500

    end serch and end zamet

--- js.js
+++ js.js
@@ -35,5 +35,44 @@ document.getElementById('closed').addEventListener('click', (ivent) =>{
     wini.style.display = 'none';
     temno.style.display = 'none'
 })
+const note_text = document.getElementById('name_note');
+const note_list = document.getElementById('zamet');
+const note = document.getElementsByClassName('note');
+document.getElementById('apply').addEventListener('click', (eve)=>{
+    eve.preventDefault();
+    const buttons = document.createElement('button')
+    buttons.className = 'buttons';
+    const text_note = note_text.value;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'note';
+    const zam = document.createElement('label');
+    const new_task = document.createElement('span');
+    new_task.className = 'text';
+    new_task.innerHTML = text_note;
+    const redakt = document.createElement('button');
+    redakt.className = 'frame'
+    const trash = document.createElement('button');
+    trash.className = 'trash'
 
+
+    note_list.appendChild(zam);
+    zam.appendChild(checkbox);
+    zam.appendChild(new_task);
+    zam.appendChild(buttons);
+    buttons.appendChild(redakt);
+    buttons.appendChild(trash);
+})
+
+const search_input = document.getElementById('poisk');
+search_input.addEventListener('input', ()=>{
+    let listNotes = document.querySelectorAll('#zamet > label >span.text');
+    for(let i = 0; i < listNotes.length; i++){
+        listNotes[i].parentElement.style.display = 'flex';
+
+        if(!listNotes[i].innerText.includes(search_input.value)){
+            listNotes[i].parentElement.style.display = 'none';
+        }
+    }
+})
 
